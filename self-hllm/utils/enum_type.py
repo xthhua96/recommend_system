1eb4b5b02c669a6c9688ee370f6e9ad04d788244
xthhua96 <xthxxx@126.com>
2024-10-18 20:46:53 +0800

    add hllm

---
+++ self-hllm/utils/enum_type.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class InputType(Enum):
+
+    SEQ = 1
+    PAIR = 2
+    AUGSEQ = 3
+
+
+class EvaluatorType(Enum):
+    """Type for evaluation metrics.
+
+    - ``RANKING``: Ranking-based metrics like NDCG, Recall, etc.
+    - ``VALUE``: Value-based metrics like AUC, etc.
+    """
+
+    RANKING = 1
+    VALUE = 2
