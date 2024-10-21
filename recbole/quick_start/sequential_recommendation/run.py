a8018c8cc593974bd788ec261d868424cef12183
xthhua96 <xthxxx@126.com>
2024-10-21 17:30:23 +0800

    update sq/test.yaml

--- recbole/quick_start/sequential_recommendation/run.py
+++ recbole/quick_start/sequential_recommendation/run.py
@@ -1,3 +1,7 @@
 from recbole.quick_start import run_recbole
 
-run_recbole(model="GRU4Rec", dataset="ml-100k", config_file_list=["test.yaml"])
+# run_recbole(model="GRU4Rec", dataset="ml-100k", config_file_list=["test.yaml"])
+parameter_dict = {
+    "train_neg_sample_args": None,
+}
+run_recbole(model="KSR", dataset="ml-100k", config_dict=parameter_dict)
