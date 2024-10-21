901a08f678daf61650f763dd684b795a1def7bb9
xthhua96 <xthxxx@126.com>
2024-10-21 17:23:10 +0800

    test .gitignore update

--- recbole/quick_start/contextaware_recommendation/run.py
+++ recbole/quick_start/contextaware_recommendation/run.py
@@ -1,3 +1,4 @@
 from recbole.quick_start import run_recbole
 
-run_recbole(model="LR", dataset="ml-100k", config_file_list=["test.yaml"])
+# run_recbole(model="LR", dataset="ml-100k", config_file_list=["test.yaml"])
+run_recbole(model="FM", dataset="ml-100k", config_file_list=["test.yaml"])
