4ec3c9993dd4b525b756bcf4ae80bc5f07bef934
xthhua96 <xthxxx@126.com>
2024-10-22 15:27:50 +0800

    add BPR

--- recbole/quick_start/sequential_recommendation/run.py
+++ recbole/quick_start/sequential_recommendation/run.py
@@ -4,4 +4,4 @@ from recbole.quick_start import run_recbole
 parameter_dict = {
     "train_neg_sample_args": None,
 }
-run_recbole(model="KSR", dataset="ml-100k", config_dict=parameter_dict)
+run_recbole(model="BERT4Rec", dataset="ml-100k", config_dict=parameter_dict)
